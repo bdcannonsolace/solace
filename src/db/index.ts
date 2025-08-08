39c3e5977a39ff2dc4d49ae286239b003b8ba3d1
bdcannon <cannon.benjamind+solace@gmail.com>
2025-08-08 02:11:28 -0700

    Updated schema. Fixes compiler errors

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,20 +1,37 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const setup = () => {
+import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
+
+type FallbackDb = {
+  select: () => {
+    from: () => any[];
+  };
+  insert: (_: unknown) => {
+    values: (_: unknown) => {
+      returning: () => any[];
+    };
+  };
+};
+
+const setup = (): PostgresJsDatabase | FallbackDb => {
   if (!process.env.DATABASE_URL) {
-    console.error("DATABASE_URL is not set");
     return {
       select: () => ({
         from: () => [],
       }),
+      insert: () => ({
+        values: () => ({
+          returning: () => [],
+        }),
+      }),
     };
-  }
+  } else {
+    const queryClient = postgres(process.env.DATABASE_URL);
+    const db = drizzle(queryClient);
 
-  // for query purposes
-  const queryClient = postgres(process.env.DATABASE_URL);
-  const db = drizzle(queryClient);
-  return db;
+    return db;
+  }
 };
 
 export default setup();
